40b95c61fe1d20ff1988e228ab197a4b3393429b
SeongTae Jeong <techsharer@outlook.com>
2026-01-10 18:36:43 +0900

    Add project files.

---
+++ win-notify-send/Program.cs
@@ -0,0 +1,75 @@
+using System.Runtime.InteropServices;
+
+namespace win_notify_send;
+
+class Program
+{
+    [DllImport("wintoast.dll", CharSet = CharSet.Unicode)]
+    public static extern int fnToastSimple(string appId, string appName, string xmlText);
+
+    [MTAThread]
+    static int Main(string[] args)
+    {
+        if (OperatingSystem.IsWindows() == false)
+        {
+            Console.WriteLine("This program only runs on Windows.");
+            Environment.Exit(1);
+            return 1;
+        }
+
+        if (args.Length == 0 || args.Length > 2)
+        {
+            Console.WriteLine("Usage: win-notify-send <message>");
+            Console.WriteLine("       win-notify-send <title> <message>");
+            Environment.Exit(1);
+            return 1;
+        }
+
+        string title;
+        string message;
+
+        if (args.Length == 1)
+        {
+            title = "win-notify-send";
+            message = args[0];
+        }
+        else
+        {
+            title = args[0];
+            message = args[1];
+        }
+
+        string toastMessage = $"""
+<toast>
+    <visual>
+        <binding template="ToastGeneric">
+            <text>{title}</text>
+            <text>{message}</text>
+        </binding>
+    </visual>
+</toast>
+""";
+
+        string appName = "win-notify-send";
+
+        // 단축 아이콘 생성 디렉터리: %USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs
+        String shortcutPath =
+            $@"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\Microsoft\Windows\Start Menu\Programs\{appName}.lnk";
+
+        if (!System.IO.File.Exists(shortcutPath))
+        {
+            // 최초 실행인 경우, AppId 등록 및 바로가기 아이콘 생성 후 5초 정도 대기
+            fnToastSimple($"kr.pe.sysnet.www.{appName}", appName, toastMessage);
+            Thread.Sleep(5000);
+        }
+
+        if (fnToastSimple($"kr.pe.sysnet.www.{appName}", appName, toastMessage) == 0)
+        {
+            Console.WriteLine("Failed to send toast notification.");
+            return 2;
+        }
+
+        Console.WriteLine("Toast notification sent successfully.");
+        return 0;
+    }
+}
---
+++ win_toast/win_toast.c
@@ -0,0 +1,38 @@
+// win_toast.cpp : Defines the exported functions for the DLL.
+//
+
+#define INITGUID
+#define COBJMACROS
+#define WIN32_LEAN_AND_MEAN
+
+#include "framework.h"
+#include "win_toast.h"
+
+#ifdef _DEBUG
+#define Assert(Cond) do { if (!(Cond)) __debugbreak(); } while (0)
+#else
+#define Assert(Cond) ((void)(Cond))
+#endif
+
+#define HR(hr) do { HRESULT _hr = (hr); Assert(SUCCEEDED(_hr)); } while (0)
+
+#include "WindowsToast.h"
+
+WINTOAST_API int fnToastSimple(wchar_t* notifyAppId, wchar_t* notifyName, wchar_t* xmlText)
+{
+    BOOL result = FALSE;
+    int length = wcslen(xmlText);
+
+    {
+        WindowsToast toast;
+        memset(&toast, 0, sizeof(WindowsToast));
+        WindowsToast_Init(&toast, notifyName, notifyAppId);
+        WindowsToast_HideAll(&toast, notifyAppId);
+
+        result = WindowsToast_ShowSimple(&toast, xmlText, length, NULL, 0);
+
+        WindowsToast_Done(&toast);
+    }
+
+    return result;
+}
